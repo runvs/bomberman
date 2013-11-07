022c927066fd50725c047faac73a470f7d036426
Laguna <laguna.ws@gmail.com>
2013-11-07 19:39:35 +0000

    adding first window with simple close events and external libs

--- code/Bomberman/Program.cs
+++ code/Bomberman/Program.cs
@@ -8,8 +8,39 @@ namespace Bomberman
 {
     class Program
     {
+
+        static void OnClose(object sender, EventArgs e)
+        {
+            // Close the window when OnClose event is received
+            SFML.Graphics.RenderWindow window = (SFML.Graphics.RenderWindow)sender;
+            window.Close();
+        }
+
+        static void OnKeyPress(object sender, SFML.Window.KeyEventArgs e)
+        {
+            if (e.Code == SFML.Window.Keyboard.Key.Escape)
+            {
+                SFML.Graphics.RenderWindow window = (SFML.Graphics.RenderWindow)sender;
+                window.Close();
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            SFML.Graphics.RenderWindow applicationWindow = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(800,600, 32), "Bomberman");
+
+            // fuddle with resizing the images later on
+            applicationWindow.Closed += new EventHandler(OnClose);
+            applicationWindow.KeyPressed += new EventHandler<SFML.Window.KeyEventArgs>(OnKeyPress);
+
+
+            while (applicationWindow.IsOpen())
+            {
+                applicationWindow.DispatchEvents();
+                applicationWindow.Clear(new SFML.Graphics.Color(200,10,10));
+                applicationWindow.Display();
+            }
         }
     }
 }
